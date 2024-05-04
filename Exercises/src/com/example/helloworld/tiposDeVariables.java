231e846d6fb527151b0e7457e18d9f751ef894de
ChristianG-ops <engbermudez@outlook.com>
2024-05-04 10:33:32 +0100

    Tipos de Variables

--- Exercises/src/com/example/helloworld/tiposDeVariables.java
+++ Exercises/src/com/example/helloworld/tiposDeVariables.java
@@ -1,13 +1,13 @@
 package com.example.helloworld;
-// Aqui vamos a ver que tipos de variables hay y como se usan
+// Aquí vamos a ver qué tipos de variables hay y como se usan
 public class tiposDeVariables {
     public static <integer> void main(String[] args) {
         System.out.println("Tipos de variables ");
         // Clases y primitivos
         String nombre = "my name"; // Aqui la variable es de tipo String
         System.out.println(nombre);
-        String nombreMetodo = "my name";
-        System.out.println("nombreMetodo con Metodo UpperCase = " + nombreMetodo.toUpperCase()); // Puedo adicionar un metodo
+        String nombreMeth = "my name";
+        System.out.println("nombreMeth con Método UpperCase = " + nombreMeth.toUpperCase()); // Puedo adicionar un método
 
         int number = 10; //Primitivo
         Integer numberInteger = 10; // Este ya es una clase
@@ -16,17 +16,17 @@ public class tiposDeVariables {
         boolean value = true;
 
         int varInside = 1; // vamos a usar dentro del if
-        if (value) {  // Si value es true - osea siempre porque arriba esta como true
+        if (value) {  // Si value es true - sea siempre porque arriba esta como true
 
             System.out.println("varInside = " + varInside);
             varInside = 12; // Puedo dar un valor diferente a la variable sin definirla de nuevo
         }
 
-        System.out.println("varInside = " + varInside); // Aqui el valor que tendria dentro del IF osea el output
+        System.out.println("varInside = " + varInside); // Aquí el valor que tendría dentro del IF o sea el output
 
-        // var - Basicamente cambia dependiendo el tipo de dado, es mas versatil
-        var num = 1;  // Aqui es un Int
-        var n1um = "camilo"; // Aqui una String
+        // var - Cambia dependiendo el tipo de dado, es más versátil
+        var num = 1;  // Aquí es un Int
+        var n1um = "camilo"; // Aquí una String
         String meth_substring = "abc".substring(2,3); // 0a 1b 2c -- Substraer del bit x hasta y
         System.out.println("c = " + meth_substring.toUpperCase()); //
 
